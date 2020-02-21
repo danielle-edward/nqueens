59595f59971c9c8c46cd01c169ccd4c73c882f53
AnthonyMarsili <56132966+AnthonyMarsili@users.noreply.github.com>
2020-02-21 12:28:19 -0500

    Added comments and file I/O

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 #CISC352 nqueens assignment 1
 import random
-
-
-#So this implementation works, problem is I haven't decided on a criteria to decide
-#when it has gotten stuck on local hill, any suggestions would be great lmao
-
-
+import os
 
 queensOnBoard = []
-
 
 class Queen:
     def __init__(self, inputCoords):
@@ -21,156 +15,109 @@ class Queen:
     def setCoords(self, inputCoords):
         self.coords = inputCoords
 
-
-def calculateConflictsQueen(curQueen):
-    x = curQueen.coords[0]
-    y = curQueen.coords[1]
-    conflictCount = 0
-
-    for i in queensOnBoard:
-        
-        if i != curQueen: 
-            if(i.coords[0] == x) or (i.coords[1] == y) or\
-               (i.coords[0] - i.coords[1]) == (x - y) or  \
-               (i.coords[0] + i.coords[1]) == (x + y):
-                    conflictCount = conflictCount + 1
-            
-    return conflictCount
-
-
-
-def calculateConflicts(curCoords):
+def calculateConflicts(curCoords): # this menthod calculates and returns the number of conflicts for a given Queen on the board
     x = curCoords[0]
     y = curCoords[1]
     conflictCount = 0
 
     for i in queensOnBoard:
-       
-        if not ((i.coords[0] == x) and (i.coords[1] == y)): 
+
+        if not ((i.coords[0] == x) and (i.coords[1] == y)):
             if (i.coords[1] == y) or\
                (i.coords[0] - i.coords[1]) == (x - y) or  \
                (i.coords[0] + i.coords[1]) == (x + y):
                     conflictCount = conflictCount + 1
-            
-    return conflictCount
-        
-    
 
-def generateBoard(n):
+    return conflictCount
+
+def generateBoard(n): # this method will generate an nxn board of n queens. It does so using a "greedy" alogorithm
+    global queensOnBoard
+    queensOnBoard = []
+
     curRow = 1
-    
-    for i in range(n):
+
+    for i in range(n): # for loop to iterate through each row
         lowestConflict = float('inf')
-        for j in range (n):
+        for j in range (n): # for loop to iterate through each col to determine where to place the queen on the row
             curColumn = j + 1
-            curConflict = calculateConflicts((curRow, curColumn))
-            if curConflict < lowestConflict :
+            curConflict = calculateConflicts((curRow, curColumn)) # calculate the conflicts that will result from placing a queen at the current spot
+            if curConflict < lowestConflict : # if it is the lowest found conflict in the row, that column is the current best column
                 bestColumn = curColumn
                 lowestConflict = curConflict
-            elif curConflict == lowestConflict:
+            elif curConflict == lowestConflict: # if it is the same as the lowest conflict, then randomly choose the previous best or the current
                 rand = random.randint(1,51)
                 if(rand < 25):
                     bestColumn = curColumn
                     lowestConflict = curConflict
-        tempQueen = Queen((curRow, bestColumn))
-        queensOnBoard.append(tempQueen)
+        tempQueen = Queen((curRow, bestColumn)) # create a Queen with the newly found best coordinates
+        queensOnBoard.append(tempQueen) # add that Queen to the queensOnBoard list
         del tempQueen
-        curRow = curRow + 1
+        curRow = curRow + 1 # move on to next row to find optimal place to put Queen on that row
 
-    for i in queensOnBoard:
-        if calculateConflicts(i.coords) > 0:
+    for i in queensOnBoard: # for loop to iterate through the origial Queens in the list
+        if calculateConflicts(i.coords) > 0: # if that Queens has any conflicts, set its Bool go True
             i.setConflict(True)
-        else:
+        else:                                # else set it to False
             i.setConflict(False)
-
-
 
 def solveBoard():
     global queensOnBoard
     n = len(queensOnBoard)
     moves = 0
 
-   
-        
-    
-    while(not all(i.conflict is False for i in queensOnBoard)):
+    while(not all(i.conflict is False for i in queensOnBoard)): # while there are still Queens with conflicts
         randQueen = random.randint(1, n-1)
-        curQueen = queensOnBoard[randQueen]
-        currentConflict = calculateConflicts(queensOnBoard[randQueen].coords)
+        curQueen = queensOnBoard[randQueen] # choose a random Queen on the board
+        currentConflict = calculateConflicts(queensOnBoard[randQueen].coords) # calculate the conflict of that Queen
 
-        if moves == 60:
+        # this helps to avoid getting stuck on local optimum
+        if moves == 60: # resets when number of moves hits 60 because it typicall takes less than 50 moves to complete any n queens problem
             queensOnBoard = []
             generateBoard(n)
             moves = 0
-          
 
-
-        
-        if queensOnBoard[randQueen].conflict is True:
-            for y in range(n):
+        if queensOnBoard[randQueen].conflict is True: # if the randomly chosen Queen has a conflict
+            for y in range(n): # iterate through all the columns
                 column = y + 1
-                
-                if column != queensOnBoard[randQueen].coords[1]:
-                    tempConflict = calculateConflicts((queensOnBoard[randQueen].coords[0], column))
-                    if tempConflict < currentConflict:
+
+                if column != queensOnBoard[randQueen].coords[1]: # if we are not on the same column as the random Queen
+                    tempConflict = calculateConflicts((queensOnBoard[randQueen].coords[0], column)) # calculate the potential conflicts if the Queen moves to this col
+                    if tempConflict < currentConflict: # if the number of potential conflicts is less than the currnet conflicts, move the Queen to that position
                         queensOnBoard[randQueen].setCoords((curQueen.coords[0], column))
                         currentConflict = tempConflict
-                        if currentConflict == 0:
+                        if currentConflict == 0: # if this new position has no conflicts, set that Queen's boolean to False
                             queensOnBoard[randQueen].setConflict(False)
-                            
-                    elif tempConflict < currentConflict:
+
+                    elif tempConflict == currentConflict: # if the number of potential conflicts is the same as the current conflicts, randonly choose the new or old position
                         rand2 = random.randint(1,51)
-                        if(rand < 25):
+                        if(rand2 < 25):
                             queensOnBoard[randQueen].setCoords((curQueen.coords[0], column))
                             currentConflict = tempConflict
-                            if currentConflict == 0:
+                            if currentConflict == 0: # if this new position has no conflicts, set that Queen's boolean to False
                                 queensOnBoard[randQueen].setConflict(False)
- 
-            moves = moves + 1
-          
-    queensOnBoard.sort(key=lambda x: x.coords[0], reverse=False)
+
+            moves = moves + 1 # a move is taken everytime the chosen Queen has conflicts and it goes through each potential column for that Queen
+
+    queensOnBoard.sort(key=lambda x: x.coords[0], reverse=False) # sort the Queens in the list by their x coordinates
     matrix = []
-    for i in queensOnBoard:
+    for i in queensOnBoard: # creates a list of the y coordinates of the Queens. The first index is row 1, the last is row n
         matrix.append(i.coords[1])
 
     return matrix
 
 
 def main():
-    #Q1 = Queen((1,4))
-    #Q2 = Queen((2,2))
-    #Q3 = Queen((3,7))
-    #Q4 = Queen((4,3))
-    #Q5 = Queen((5,6))
-    #Q6 = Queen((6,8))
-    #Q7 = Queen((7,5))
-    #Q8 = Queen((8,3))
-    #Q8.setConflict(True)
+    cwd = os.getcwd().replace('\\', '/') # gets the current working directory of this python file
 
-    #queensOnBoard.append(Q1)
-    #queensOnBoard.append(Q2)
-    #queensOnBoard.append(Q3)
-    #queensOnBoard.append(Q4)
-    #queensOnBoard.append(Q5)
-    #queensOnBoard.append(Q6)
-    #queensOnBoard.append(Q7)
-    #queensOnBoard.append(Q8)
+    f1 = open(cwd + "/nqueens.txt", mode='r') # opens the nqueens file to read the n's from
 
- 
- 
+    f2 = open(cwd + "/nqueens_out.txt", mode='w+') # creates an output file for the matrices to be placed in
 
-    #print("Should be 0: " + str(calculateConflicts(Q1.coords)))
-    #print("Should be 1: " + str(calculateConflicts(Q2)))
-    #print("Should be 0: " + str(calculateConflicts(Q3.coords)))
-    #print("Should be 2: " + str(calculateConflicts(Q4)))
+    for n in f1:
+        print(n)
+        generateBoard(int(n)) # a board is generated for each n in the input file
+        f2.write("%s\n" % str(solveBoard())) # writes the solution to each size n to the output file
 
-    generateBoard(8)
-
-    #for i in queensOnBoard:
-        #print(str(i.coords) + " is conflicting: " + str(i.conflict))
-    answer = solveBoard()
-    print(answer)
+    f2.close()
 
 main()
-
-    
